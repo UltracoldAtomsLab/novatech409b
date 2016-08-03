1c38eb81d868986c7999383f027ea593c4761efd
AceChen <acechen@cirx.org>
2016-08-03 11:11:04 -0400

    revise table mode

--- run.py
+++ run.py
@@ -5,9 +5,7 @@ import time
 dev = driver.Novatech409B("/dev/tty.usbserial-FTDCTBEZ")
 dev.set_freq(int(sys.argv[1]), float(sys.argv[2]))
 
-# table mode
-#dev.table_write(0, 1e7, 1000, 1e7, 1000)
-#dev.table_write(1, 1e8, 1000, 1e8, 1000)
+# table mode. frequency in MHz
+#dev.table_write(0, 130, 1000, 130, 1000)
+#dev.table_write(1, 142, 1000, 142, 1000)
 #dev.table_start()
-#time.sleep(2)
-#dev.table_next()
