54c912439d32f460d33fb6e9cd2fc1f61fc7b373
AceChen <acechen@cirx.org>
2016-09-23 14:04:06 -0400

    test table mode

--- run.py
+++ run.py
@@ -3,9 +3,12 @@ import sys
 import time
 
 dev = driver.Novatech409B("/dev/tty.usbserial-FTDCTBEZ")
-dev.set_freq(int(sys.argv[1]), float(sys.argv[2]))
+
+# static. frequency in MHz
+#dev.set_freq(int(sys.argv[1]), float(sys.argv[2]))
 
 # table mode. frequency in MHz
-#dev.table_write(0, 130, 1000, 130, 1000)
-#dev.table_write(1, 142, 1000, 142, 1000)
-#dev.table_start()
+dev.table_write(0, 130, 1000, 130, 1000)
+dev.table_write(1, 145, 1000, 145, 1000)
+#dev.table_write(2, 130, 1000, 130, 1000)
+dev.table_start()
